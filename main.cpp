b347784d7b205d1c4c577296308f8ce35e532ae9
Karen Dantas <karennobre74@gmail.com>
2024-12-17 16:40:32 -0300

    create cache and function to count the steps

--- main.cpp
+++ main.cpp
@@ -1,110 +1,61 @@
 #include <iostream>
+#include <vector>
+#include <chrono>
 using namespace std;
-#define TAM 401
-//testando com 200 items tam = 401
+const long long int maxTam = 1000000;
 
-long long int sequence_to_1 (long long int number, long quant){
-    if (number == 1){
-        return quant;
-    };
-    if (number % 2 == 0){
-        return sequence_to_1(number / 2, quant+ 1);
+//cache 
+vector<long long int> cache (maxTam, 0);
+
+//passos 
+//1 - criar cache, vetor para cada número, tamanho de 100000
+//2 - Verificar se o número já existe no cache 
+//3- Se existe não precisa continuar a fazer os passos do número
+//4 - atualizar cache
+
+long long int sequenciaAte1(long long int n){
+
+    int sequenciaTotal = 1;
+
+    if (n == 1){
+        return 1;
+    }
+
+    if (n < maxTam && cache[n] != 0) return cache[n];
+
+    if (n % 2 == 0){
+        sequenciaTotal += sequenciaAte1(n/2); 
     }else{
-        return sequence_to_1((3*number) + 1, quant+ 1);
+        sequenciaTotal += sequenciaAte1((n*3)+1);
     }
 
+    //atualizando cache
+    if(n < maxTam) cache[n] = sequenciaTotal;
+    return sequenciaTotal;
+ 
 };
-
-
-//lista
-class Node{
-public:
-    long long int data;
-    Node* next;
-    Node(long long int d):data(d){}
-};
-
-class LinkedList{
-public:
-    Node* head;
-    long long int tam;
-};
-
-void initializaLinkedList(LinkedList* l){
-    l->head = NULL;
-    l->tam = 0;
-};
-
-void insertLinkedList(LinkedList* list, long long int data){
-    Node* newNode = new Node(data);
-
-    newNode->next = list->head;
-    list->head = newNode;
-    list->tam++;
-
-};
-
-int searchLinkedList(LinkedList* list, long long int value){
-    Node* temp = list->head;
-
-    while(temp && temp->data != value ){
-        temp = temp->next;
-        if (temp) {
-            return temp->data;
-        }
-    }
-    return 0;
-
-};
-
-int printLinkedList(LinkedList* list){
-    Node* temp = list->head;
-    while(temp){
-        cout<<temp->data<<" ";
-        temp = temp->next;
-    }
-};
-
-void initialize_hash(LinkedList t[]){
-    for (int i = 0; i < TAM; i++){
-        initializaLinkedList(&t[i]);
-    }
-};
-
-int func_hash(long long int value){
-    return value % TAM;
-};
-
-void insert_hash(LinkedList t[], long long int value){
-    long long int id = func_hash(value);
-    insertLinkedList(&t[id], value);
-
-};
-
-int search_hash(LinkedList t[], long long int value){
-    long long int id = func_hash(value);
-    return searchLinkedList(&t[id], value);
-};
-
-void print_hash(LinkedList t[]) {
-    for ( int i = 0; i < TAM; i++){
-        cout << i << "=";
-        printLinkedList(&t[i]);
-        cout << endl;
-    };
-};
-
 
 int main() {
 
-  long long int quant = 0;
-  LinkedList table[TAM];
-  initialize_hash(table);
-  for (int i = 1; i < 200; i++){
-      insert_hash(table, sequence_to_1(i, quant));
-  };
-  print_hash(table);
+    int sequenciaAtual = 0;
+    int sequenciaMaxima = 0;
+    int maiorNumero = 0;
+    auto tempo_inicio = chrono::high_resolution_clock::now();
+    for (int i =1; i<= maxTam; i++){
+        sequenciaAtual = sequenciaAte1(i);
+        if (maiorNumero == 0 ) maiorNumero = i;
+        else {
+            if (sequenciaAtual > sequenciaMaxima){
+                sequenciaMaxima = sequenciaAtual;
+                maiorNumero = i;
+            }
+        }
+    }
 
-
+    auto tempo_fim = chrono::high_resolution_clock::now();
+    long long int tempo_total = chrono::duration_cast<chrono::milliseconds>(tempo_fim - tempo_inicio).count();
+    cout << "Maior número:" << maiorNumero << endl;
+    cout << "Sequencia:" << sequenciaMaxima << endl;
+    cout << "Tempo: " << tempo_total;
   return 0;
 }
